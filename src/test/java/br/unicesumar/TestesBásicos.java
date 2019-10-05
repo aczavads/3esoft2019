0bfc6b6bcc64d9d047ebbddd012ef778ccc2e196
aczavads@gmail.com <aczavads@gmail.com>
2019-10-04 21:23:47 -0300

    Mais testes

--- src/test/java/br/unicesumar/TestesBásicos.java
+++ src/test/java/br/unicesumar/TestesBásicos.java
@@ -41,5 +41,52 @@ public class TestesBásicos {
 		assertEquals(201, maior01);
 		assertEquals(400, maior02);
 	}
+		
+	@Test(expected=RuntimeException.class)
+	public void testar_encontrar_maior_valor_em_array_vazio01() {
+		Utilidades.maior(new int[] {});
+	}
+	
+	@Test
+	public void testar_encontrar_maior_valor_em_array_vazio02() {
+		try {
+			Utilidades.maior(new int[] {});
+			fail("Deveria ter gerado uma exceção!");
+		} catch (RuntimeException e) {	
+		} catch (Exception e) {
+			fail("Deveria ser uma RuntimeException.");
+		}
+	}
+	
+	@Test
+	public void testar_total_do_pedido() {
+		Pedido p1 = new Pedido(125); //Pedido(numero)
+		p1.adicionarItem("Omo",10.00, 20.00, 0.00); // (produto, quantidade, valorUnitario, descontoPercentual)
+		p1.adicionarItem("Soja",20.00, 25.00, 10.00); // (produto, quantidade, valorUnitario, descontoPercentual)
+		
+		assertEquals(650.00, p1.getValorTotal());
+		assertEquals(2, p1.contarItens());
+		assertEquals(10.00, p1.getItem("Omo").getQuantidade());
+		assertEquals(20.00, p1.getItem("Soja").getQuantidade());
+
+		Pedido p2 = new Pedido(126); //Pedido(numero)
+		p2.adicionarItem("Cuscuz",1.00, 2.50, 0.00); // (produto, quantidade, valorUnitario, descontoPercentual)
+		p2.adicionarItem("Jambu",2.00, 10.00, 20.00); // (produto, quantidade, valorUnitario, descontoPercentual)
+		p2.adicionarItem("Omo",1.00, 1.00, 0.00); // (produto, quantidade, valorUnitario, descontoPercentual)
+		
+		assertEquals(19.50, p2.getValorTotal());
+		assertEquals(3, p2.contarItens());
+		assertEquals(1.00, p2.getItem("Cuscuz").getQuantidade());
+		assertEquals(2.00, p2.getItem("Jambu").getQuantidade());
+		assertEquals(1.00, p2.getItem("Omo").getQuantidade());
+		
+	}
 
 }
+
+
+
+
+
+
+
--- src/test/java/br/unicesumar/Utilidades.java
+++ src/test/java/br/unicesumar/Utilidades.java
@@ -5,5 +5,28 @@ public class Utilidades {
 	public static int somar(int v1, int v2) {
 		return v1+v2;
 	}
+	
+	public static int somar(int[] valores) {
+		int soma = 0;
+		
+		for (int v : valores) {
+			soma += v;
+		}
+		
+		return soma;
+	}
+	
+	public static int maior(int[] valores) {
+		if (valores == null || valores.length == 0) {
+			throw new RuntimeException("Array vazio!");
+		}
+		int maior = valores[0];
+		for (int valor : valores) {
+			if (valor > maior) {
+				maior = valor;
+			}
+		}
+		return maior;
+	}
 
 }
