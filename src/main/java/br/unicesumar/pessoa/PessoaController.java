f4f65ae8ba41dcc6b1cf0b6480d9593251c1fc2e
arthur.zavadski <aczavads@gmail.com>
2019-10-11 22:19:22 -0300

    Exemplo final de mapeamento de hierarquias com persistência

--- src/main/java/br/unicesumar/pessoa/PessoaController.java
+++ src/main/java/br/unicesumar/pessoa/PessoaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,21 @@ public class PessoaController {
 	@GetMapping
 	public List<Pessoa> get() {
 		return repo.findAll();
+	}
+	
+	@PostMapping("/fisicas")
+	public String post(@RequestBody Fisica nova) {
+		return salvar(nova);
+	}
+	
+	@PostMapping("/juridicas")
+	public String post(@RequestBody Juridica nova) {
+		return salvar(nova);
+	}
+	
+	private String salvar(Pessoa nova) {
+		repo.save(nova);
+		return nova.getId();
 	}
 	
 
